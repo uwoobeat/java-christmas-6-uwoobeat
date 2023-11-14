e90524303e2595c0ce10f61214ea237789ae0017
uwoobeat <desktop3711@gmail.com>
2023-11-14 21:33:23 +0900

    feat: 할인 정책 팩토리는 가능한 모든 할인 정책을 가진다

---
+++ src/main/java/christmas/discountpolicy/domain/DiscountPolicyFactory.java
@@ -0,0 +1,33 @@
+package christmas.discountpolicy.domain;
+
+import christmas.common.domain.MenuType;
+import christmas.common.domain.Money;
+import christmas.order.domain.Order;
+import java.time.LocalDate;
+
+public enum DiscountPolicyFactory {
+    ELAPSED(new ElapsedDayDiscountPolicy(Constants.ELAPSED_START_DATE, Constants.ELAPSED_DISCOUNT_BASE,
+            Constants.ELAPSED_DISCOUNT_STEP)),
+    DESSERT(new MenuTypeDiscountPolicy(MenuType.DESSERT, Constants.DISCOUNT_DISCOUNT_UNIT)),
+    MAIN(new MenuTypeDiscountPolicy(MenuType.MAIN, Constants.MAIN_DISCOUNT_UNIT)),
+    SPECIAL(new FixedDiscountPolicy(Constants.SPECIAL_DISCOUNT_AMOUNT));
+
+    private final DiscountPolicy discountPolicy;
+
+    DiscountPolicyFactory(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money discount(Order order) {
+        return discountPolicy.discount(order);
+    }
+
+    public static class Constants {
+        public static final LocalDate ELAPSED_START_DATE = LocalDate.of(2023, 12, 1);
+        public static final Money ELAPSED_DISCOUNT_BASE = new Money(1000);
+        public static final Money ELAPSED_DISCOUNT_STEP = new Money(100);
+        public static final Money DISCOUNT_DISCOUNT_UNIT = new Money(2023);
+        public static final Money MAIN_DISCOUNT_UNIT = new Money(2023);
+        public static final Money SPECIAL_DISCOUNT_AMOUNT = new Money(1000);
+    }
+}
