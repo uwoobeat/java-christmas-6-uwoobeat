00e648dad094d996d6e7fe29c95be6319de903fd
uwoobeat <desktop3711@gmail.com>
2023-11-15 03:19:42 +0900

    feat: 이벤트를 만들 수 있다

---
+++ src/main/java/christmas/event/domain/Event.java
@@ -0,0 +1,32 @@
+package christmas.event.domain;
+
+import christmas.common.domain.Money;
+import christmas.order.domain.Order;
+
+public abstract class Event {
+
+    public static final Money MINIMUM_ORDER_PRICE = new Money(10000);
+    private final String name;
+    private final EventPeriod period;
+
+    public Event(String name, EventPeriod period) {
+        this.name = name;
+        this.period = period;
+    }
+
+    private boolean validatePeriod(Order order) {
+        return period.isWithin(order.getOrderedAt());
+    }
+
+    private boolean validateTotalPrice(Order order) {
+        return order.calculateTotalPrice().isGreaterOrEqual(MINIMUM_ORDER_PRICE);
+    }
+
+    public final boolean isAppliable(Order order) {
+        return validateTotalPrice(order) &&
+                validatePeriod(order) &&
+                validateAdditionalConditions(order);
+    }
+
+    protected abstract boolean validateAdditionalConditions(Order order);
+}
