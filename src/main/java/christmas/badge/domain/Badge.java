cb6e110ea706063b9b8d52dd092a778bbd17bc58
uwoobeat <desktop3711@gmail.com>
2023-11-15 16:16:17 +0900

    feat: 배지를 만들 수 있다

--- src/main/java/christmas/badge/domain/Badge.java
+++ src/main/java/christmas/badge/domain/Badge.java
@@ -1,4 +1,23 @@
 package christmas.badge.domain;
 
+import christmas.badgepolicy.domain.BadgeIssuePolicy;
+import christmas.reservation.domain.MoneyInfo;
+
 public class Badge {
+
+    private final String name;
+    private final BadgeIssuePolicy policy;
+
+    public Badge(String name, BadgeIssuePolicy policy) {
+        this.name = name;
+        this.policy = policy;
+    }
+
+    public boolean isIssuable(MoneyInfo info) {
+        return policy.isIssuable(info);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
