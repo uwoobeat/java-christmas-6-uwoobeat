58c2721ec26553442f1a10af85f62c4dbbe9df9d
uwoobeat <desktop3711@gmail.com>
2023-11-15 19:50:34 +0900

    feat: 이벤트 플래너를 실행한다

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.common.manager.EventPlanner;
+import christmas.common.view.InputView;
+import christmas.common.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        EventPlanner eventPlanner = new EventPlanner(new InputView(), new OutputView());
+        eventPlanner.run();
     }
 }
